d91f2f1b05e6fd4644bb601719deb51a5d953252
Vinicius Higuchi <vinicius-higuchi@outlook.com>
2020-01-29 00:33:28 -0300

    Click movement, Follow Camera
    Added:
    Click to move
    Camera following player
    Prefab folder (organizing)
    Edited:
    Shadows graphic setting, from 2 to 4 cascades

---
+++ Assets/Scripts/DinamicObstacle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DinamicObstacle : MonoBehaviour
+{
+    public float change = 9;
+    public float period = 0.5f;
+    public float time = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time = Time.deltaTime + time;
+        transform.localPosition = transform.localPosition + (transform.right * change * Time.deltaTime);
+        if(time > period)
+        {
+            change = -change;
+            time = 0f;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+            MoveToCursor();
+        
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if(hasHit)
+            GetComponent<UnityEngine.AI.NavMeshAgent>().destination = hit.point;
+    }
+}
