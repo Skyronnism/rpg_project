8de8958329ea48e7bd8d9de2642154aa29ed0b57
Vinicius Higuchi <vinicius-higuchi@outlook.com>
2020-01-31 15:17:47 -0300

    Finished basic movement
    Added
    Animation system
    -controller
    -blend tree
    -code match with velocity
    Finished movement system
    -changed MouseButtonDown to MouseButton, make the movement follow the mouse hold
    -added camera follow to LateUpdate method to get a consistent result of order of execution.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,14 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButton(0))
             MoveToCursor();
-        
+        UpdateAnimator();
     }
 
     private void MoveToCursor()
@@ -17,6 +18,15 @@ public class Mover : MonoBehaviour
         RaycastHit hit;
         bool hasHit = Physics.Raycast(ray, out hit);
         if(hasHit)
-            GetComponent<UnityEngine.AI.NavMeshAgent>().destination = hit.point;
+            GetComponent<NavMeshAgent>().destination = hit.point;
     }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+    }
+
 }
